d973610a3ae672a8b4c0586c77b7465e52500339
Gilbert Hada <gilberthada08@gmail.com>
2023-09-15 08:11:22 +0000

    removing element

---
+++ 8kyu/removing-element.js
@@ -0,0 +1,23 @@
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+// my solution
+
+function removeEveryOther(arr) {
+  return arr.filter(function (elem, index) {
+    return index % 2 === 0;
+  });
+}
+
+// alternate solution
+function removeEveryOther(arr) {
+  var newArr = [];
+  for (var i = 0; i < arr.length; i += 2) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
+}
